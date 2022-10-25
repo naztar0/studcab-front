47d3f64fa38b35483ab59ed5d1dfae4f2ad7e9fa
Nazar Taran <nazar.taran.id@gmail.com>
2022-10-26 01:36:11 +0300

    Add auth headers to requests

--- src/requests/users.ts
+++ src/requests/users.ts
@@ -3,19 +3,27 @@ import axios from 'axios';
 export const fetchUserProfile = async (user: number) => axios({
   method: 'get',
   url: `${import.meta.env.VITE_APP_API_URL}/api/users/${user}/profile`,
+  withCredentials: true,
+  headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
 });
 
 export const fetchUserRecordBook = async (user: number, semester: number) => axios({
   method: 'get',
   url: `${import.meta.env.VITE_APP_API_URL}/api/users/${user}/record-book/${semester}`,
+  withCredentials: true,
+  headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
 });
 
 export const fetchUserRating = async (user: number, semester: number) => axios({
   method: 'get',
   url: `${import.meta.env.VITE_APP_API_URL}/api/users/${user}/rating/${semester}`,
+  withCredentials: true,
+  headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
 });
 
 export const fetchUserSyllabus = async (user: number, semester: number) => axios({
   method: 'get',
   url: `${import.meta.env.VITE_APP_API_URL}/api/users/${user}/syllabus/${semester}`,
+  withCredentials: true,
+  headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
 });
